c91e55df2c79ff91ce5872f9609aa30d640af374
Alain Meunier <alain@maep.fr>
2026-01-17 16:40:23 +0100

    Add Logo and Photo images floating over header with bottom overflow

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from './Header.module.css';
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      {/* Têtière bleue */}
+      <div className={styles.logoContainer}>
+        <Image
+          src="/images/Logo.png"
+          alt="Logo Malain et possible"
+          width={150}
+          height={150}
+          className={styles.logo}
+          priority
+        />
+      </div>
+      <div className={styles.photoContainer}>
+        <Image
+          src="/images/Photo.png"
+          alt="Photo Alain Meunier"
+          width={150}
+          height={150}
+          className={styles.photo}
+          priority
+        />
+      </div>
     </header>
   );
 };
