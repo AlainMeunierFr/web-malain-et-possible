8fadf78fea49c7bd4195a1574bdef7690a32387c
Alain Meunier <alain@maep.fr>
2026-01-17 16:23:38 +0100

    Restore Hello World page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,3 @@
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import Services from '../components/Services';
-import About from '../components/About';
-import Footer from '../components/Footer';
-
 export default function Page() {
-  return (
-    <main>
-      <Header />
-      <Hero />
-      <Services />
-      <About />
-      <Footer />
-    </main>
-  );
+  return <h1>Hello World</h1>;
 }
